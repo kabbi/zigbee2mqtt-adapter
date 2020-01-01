76c15d3695d9e9a09f5053de7d4f4f959bdeda5a
Andre Suzanovich <supius@gmail.com>
2020-01-01 19:03:48 +0100

    fix package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+rm -rf node_modules
+npm install --production
 rm -f SHA256SUMS
-sha256sum -- package.json manifest.json *.js LICENSE README.md > SHA256SUMS
-npm pack
-
+sha256sum manifest.json package.json *.js LICENSE README.md > SHA256SUMS
+rm -rf node_modules/.bin
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+TARFILE=$(npm pack)
+tar xzf ${TARFILE}
+cp -r node_modules ./package
+tar czf ${TARFILE} package
+rm -rf package
+echo "Created ${TARFILE}"
