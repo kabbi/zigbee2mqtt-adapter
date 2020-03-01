bc1a654542ebb47e1c827a3d8237bafe4f0d2187
Peter Sykora <psykora@gmail.com>
2020-03-01 17:47:16 +0100

    [impl] Add support for aquara weather sensor
    [fix] Correct 'modelId' property -> 'modelID' to make it work with latest zigbee2mqtt messages
    [refac] Make the adaptor work with default zigbee2mqtt configuration without the 'include_device_information' option

--- devices.js
+++ devices.js
@@ -126,4 +126,41 @@ module.exports = {
       },
     },
   },
+  'lumi.weather': {
+    name: 'Xiaomi Aquara Temperature & Humidity & Pressure Sensor',
+    '@type': ['TemperatureSensor'],
+    properties: {
+      battery: {
+        type: 'integer',
+        unit: 'percent',
+        minimum: 0,
+        maximum: 100,
+        readOnly: true,
+      },
+      linkquality: {
+        type: 'integer',
+        readOnly: true,
+      },
+      temperature: {
+        type: 'number',
+        '@type': 'TemperatureProperty',
+        unit: 'degree celsius',
+	multipleOf: 0.1,
+        readOnly: true,
+      },
+      humidity: {
+        type: 'number',
+        unit: 'percent',
+	multipleOf: 0.5,
+        readOnly: true,
+      },
+      pressure: {
+        type: 'number',
+        unit: 'hPa',
+	multipleOf: 0.5,
+        readOnly: true,
+      },
+    },
+  },
+
 };
