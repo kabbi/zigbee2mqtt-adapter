969e0719907a6096aa26591d72e78d0c659f972f
Andre Suzanovich <andre.suzanovich@doverfs.com>
2020-07-07 13:35:40 +0200

    add MiJia Honeywell smoke detector

--- devices.js
+++ devices.js
@@ -214,5 +214,37 @@ module.exports = {
         readOnly: true,
       },
     },
+  },
+  "JTYJ-GD-01LM/BW": {
+    name: 'MiJia Honeywell smoke detector',
+    '@type': ['BinarySensor'],
+    properties: {
+      battery: {
+        type: 'integer',
+        unit: 'percent',
+        minimum: 0,
+        maximum: 100,
+        readOnly: true,
+      },
+      voltage: {
+        type: 'integer',
+        '@type': 'VoltageProperty',
+        unit: 'volt',
+        readOnly: true,
+      },
+      "smoke_density": {
+        type: 'integer',
+        readOnly: true,
+      },
+      "smoke": {
+        type: 'boolean',
+        '@type': 'BooleanProperty',
+        readOnly: true,
+      },
+      linkquality: {
+        type: 'integer',
+        readOnly: true,
+      },
+    },
   }
 };
