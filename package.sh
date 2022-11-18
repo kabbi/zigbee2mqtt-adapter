168d574c73decf7115e2d32def2256a3b1795cc7
flatsiedatsie <unfold@gmail.com>
2022-11-18 16:11:05 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,6 +3,9 @@
 RELEASE_VERSION="$1"
 NODE_VERSION="$2"
 
+echo "Release version $RELEASE_VERSION"
+echo "Node version: $NODE_VERSION"
+
 rm -rf node_modules
 
 npm install --production
@@ -21,9 +24,12 @@ shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 
 rm -rf SHA256SUMS package
 
-# It needs to become something like this: zigbee2mqtt-adapter-${{ env.RELEASE_VERSION }}-v${{ matrix.node-version }}.tgz
+# It needs to become something like this: zigbee2mqtt-adapter-${{ env.RELEASE_VERSION }}-v${{ matrix.node-vers>
+
+echo "renaming files"
+echo "old name: $TARFILE"
 
 mv ${TARFILE} zigbee2mqtt-adapter-${RELEASE_VERSION}-v${NODE_VERSION}.tgz
-mv ${TARFILE}.sha256sum zigbee2mqtt-adapter-${RELEASE_VERSION}-v${NODE_VERSION}.tgz
+mv ${TARFILE}.sha256sum zigbee2mqtt-adapter-${RELEASE_VERSION}-v${NODE_VERSION}.tgz.shasum
 
 exit 0
